ad2cae96d9f2191db6dc76d8c0c270b003532ff3
Yuvraj Singh <yuvrajyuvi72608@gmail.com>
2025-07-24 18:13:17 +0530

    initail commit

---
+++ task_manager/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Handle from "./components/Handle"
+import "./App.css"
+
+
+function App() {
+  
+  return (
+    <>
+    <h2>welcome</h2>
+    <Handle></Handle>
+    </>
+  )
+  
+}
+
+export default App
---
+++ task_manager/src/components/Handle.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+function Handle(){
+ 
+  const[toDo,setToDo]=useState(["read","write"])
+  const [inputValue, setInputValue] = useState("");
+
+  function trackChange(event){
+    setInputValue(event.target.value);
+
+  }
+  function addItem(){
+   if (inputValue.trim() !== "") {
+      setToDo([...toDo, inputValue]);  // Add input to the list
+      setInputValue("");               // Clear input
+    }
+
+  }
+  function modifyTodo(indexToDelete){
+    const updatedList=toDo.filter((_,index)=>index !=indexToDelete);
+    setToDo(updatedList);
+
+  }
+  
+
+  return (
+    <div className="outer">
+      <p>Task To Be DOne</p>
+      <input style={{height:'30px' ,width:'300px'}} type="text" placeholder="Enter task to be done" value={inputValue} onChange={trackChange} />
+      <button style={{background:"green",height:'30px' ,width:'50px'}}  onClick={addItem}> Add</button>
+      <br /> <br />
+      <li>
+        completing pending task
+      </li>
+      <ul>
+        {toDo.map((task,index)=>{
+         return <li key={index}>{task} <br />   <button style={{background:"red"}} onClick={()=>modifyTodo(index)}>delete</button></li>
+        })}
+      </ul>
+    </div>
+  )
+    
+  
+
+}
+export default Handle;
